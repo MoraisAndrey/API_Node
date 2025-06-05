5f27db2bb59f9ef8ab19f7ff909f64bc890c5659
Andrey <mooraisandrey@gmail.com>
2025-06-05 20:31:44 -0300

    Correção Bug async function

--- streams/stream-csv.js
+++ streams/stream-csv.js
@@ -6,13 +6,10 @@ const csvPath = new URL('./tasks.csv', import.meta.url);
 const stream = fs.createReadStream(csvPath);
 
 const csvParse = parse({
-  delimiter: '/t',
+  delimiter: '\t',
   skipEmptyLines: true,
   fromLine: 2
 });
-
-async function run() {
-  console.log('Iniciando leitura do CSV...');
 
   async function run() {
     const linesParse = stream.pipe(csvParse);
@@ -33,9 +30,6 @@ async function run() {
       
     }
   }
-
-}
-
 run()
 function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
